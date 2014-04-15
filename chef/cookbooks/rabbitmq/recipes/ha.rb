3b4256a7b13c9893952220a81ef5c1475eadab1e
Adam Spiers <aspiers@suse.com>
2014-04-16 00:01:47 +0100

    allow VIP and filesystem to start in parallel
    Any unnecessary ordering between the VIP and filesystem resources
    can cause problems during the chef-client run.
    
    (cherry picked from commit 7ecd3a989d8751f535239e44bc59ae55a1582289)
    
    Conflicts:
    	chef/cookbooks/rabbitmq/recipes/ha.rb

--- chef/cookbooks/rabbitmq/recipes/ha.rb
+++ chef/cookbooks/rabbitmq/recipes/ha.rb
@@ -133,7 +133,7 @@ end
 if node[:rabbitmq][:ha][:storage][:mode] == "drbd"
   pacemaker_colocation "col-#{fs_primitive}" do
     score "inf"
-    resources [fs_primitive, "#{ms_name}:Master"]
+    resources "#{fs_primitive} #{ms_name}:Master"
     action :create
   end
 
@@ -294,13 +294,13 @@ if node[:rabbitmq][:ha][:storage][:mode] == "drbd"
 
   pacemaker_colocation "col-#{service_name}" do
     score "inf"
-    resources primitives
+    resources "( #{vip_primitive} #{fs_primitive} ) #{service_name}"
     action :create
   end
 
   pacemaker_order "o-#{service_name}" do
     score "Mandatory"
-    ordering "#{primitives.join(" ")}"
+    ordering "( #{vip_primitive} #{fs_primitive} ) #{service_name}"
     action :create
     # This is our last constraint, so we can finally start service_name
     notifies :run, "execute[Cleanup #{service_name} after constraints]", :immediately
